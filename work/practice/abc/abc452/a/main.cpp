69b202e72eee2febf91d23744663b4808339a966
Junya Hiramura <hiramurajunya@gmail.com>
2026-06-08 20:07:15 +0900

    練習問題をコミット

--- work/practice/abc/abc452/a/main.cpp
+++ work/practice/abc/abc452/a/main.cpp
@@ -5,12 +5,8 @@ int main() {
     int m, d;
     cin >> m >> d;
 
-    if ((m == 1) && (d == 7) ||
-        (m == 3) && (d == 3) ||
-        (m == 5) && (d == 5) ||
-        (m == 6) && (d == 7) ||
-        (m == 7) && (d == 9)) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    set<pair<int, int>> array = {{1, 7}, {3, 3}, {5, 5}, {7, 7}, {9, 9}};
+     cout << (array.count({m, d}) ? "Yes" : "No") << endl;
 
     return 0;
 }
