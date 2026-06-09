305ff6fdb0650ba02a3f92df86cb2ae951837e72
Junya Hiramura <hiramurajunya@gmail.com>
2026-06-09 21:38:04 +0900

    練習問題をコミット

--- work/practice/abc/abc452/b/main.cpp
+++ work/practice/abc/abc452/b/main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+    int h, w;
+    cin >> h >> w;
+    // 上段
+    for (int i = 0; i < w; i++) cout << "#";
+    cout << "\n";
+
+    // 中段
+    for (int i = 0; i < h - 2; i++) {
+        for (int j = 0; j < w; j++) {
+            if (j == 0 || j == w - 1) cout << "#";
+            else cout << ".";
+        }
+        cout << "\n";
+    }
+    // 下段
+    for (int i = 0; i < w; i++) cout << "#";
+    cout << "\n";
+
     return 0;
 }
